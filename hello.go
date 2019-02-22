1e4b7af0587804127b14156e9176bbb73298b1c0
Mike Mikhailov <webtask@gmail.com>
2019-02-22 12:29:03 +0300

    release 1.0.0

--- hello.go
+++ hello.go
@@ -2,7 +2,11 @@ package helloworld
 
 import "fmt"
 
+const (
+	packageVersion = "1.0.0"
+)
+
 // HelloWorld - print greeting
 func HelloWorld() {
-	fmt.Printf("%s\n", "Hello world")
+	fmt.Printf("%s (%s)!\n", "Hello world", packageVersion)
 }
